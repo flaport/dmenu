21c13394764a8e1ff775ddf644ceeea3498bc0c7
flaport <floris.laporte@gmail.com>
2019-06-12 09:43:57 +0200

    different default colors

--- config.def.h
+++ config.def.h
@@ -5,14 +5,20 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* colors */
+static const char col_fg[]      = "#00ad9c";
+static const char col_bg[]      = "#fdf6e3";
+static const char col_black[]   = "#222222";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+/*                      fg         bg       */
+	[SchemeNorm] = {    col_fg,    col_bg },
+	[SchemeSel]  = {    col_bg,    col_fg },
+	[SchemeOut]  = {    col_black, col_fg },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
