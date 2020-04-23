627a17d12cf49c58d7d75b5e3482496c31e13fa2
flaport <floris.laporte@gmail.com>
2020-04-23 22:51:20 +0200

    trust fontconfig

--- config.def.h
+++ config.def.h
@@ -7,13 +7,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "monospace:size=14:antialias=true:autohint=true",
-	"FontAwesome:pixelsize=14:antialias=true:autohint=true",
-	"Pomodoro:pixelsize=14:antialias=true:autohint=true",
-	"Octicons:pixelsize=14:antialias=true:autohint=true",
-	"Icomoon:pixelsize=14:antialias=true:autohint=true",
-	"Hack Nerd Font:pixelsize=14:antialias=true:autohint=true",
-	"PowerlineSymbols:pixelsize=14:antialias=true:autohint=true",
+    "monospace:pixelsize=13:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
