782d34d07c41e13e162d04a30eda6e187734a74f
flaport <floris.laporte@gmail.com>
2020-04-25 19:50:34 +0200

    increase font size

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "monospace:pixelsize=13:antialias=true:autohint=true",
+    "monospace:pixelsize=17:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
